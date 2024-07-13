4148ef918b8c4d718196a2ecdae3a31facc540fb
Alexander-Puv <sansano4sana@gmail.com>
2024-07-13 14:45:20 +0300

    Gear Icon X section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import galaxyNote92 from './assets/galaxyNote92.png'
 import galaxyTabS4 from './assets/galaxyTabS4.png'
 import Product from "./components/UI/Product"
 import Connect from "./components/Connect"
+import GearIconX from "./components/GearIconX"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Product image={galaxyNote9} title='The new super powerful Galaxy Note9' text='Meet the latest member of the Galaxy family.' btnText='Buy now' bgColor='1' />
       <Connect />
       <Product image={galaxyNote92} title='Galaxy Note9: See what else it can do.' text="The Galaxy Note9 is packed with features to up your photo game and connect you more deeply with the things that are important to you. See how it's redefining the smartphone. Meet the latest member of the Galaxy family." btnText='Learn more' bgColor='2' imageSide='left' />
+      <GearIconX />
       <Product image={galaxyTabS4} title='Tablet mobility.PC power.' text='The Galaxy Tab S4 gives you a stunningly brilliant screen, a PC experience in a tablet and more.' btnText='Buy now' bgColor='3' />
     </main>
   )
---
+++ src/components/GearIconX.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import gearX from '../assets/gearX.jpg'
+
+const GearIconX = () => {
+  return (
+    <section className='gearIconX'>
+      <h3>Gear IconX</h3>
+      <p>The Gear IconX are cutting-edge wireless earbuds—as well as a portable music library,4 fitness tracker, and personal coach.5 Whether standalone or connected to your Note9,6 you can listen to your playlists and podcasts, maximize your workouts, and log your progress, all without touching your phone.</p>
+      <img src={gearX} alt='Gear IconX' />
+    </section>
+  )
+}
+
+export default GearIconX
